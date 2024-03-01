f559aa98fc1913d29e097b9663210c5fd209a01d
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-03-01 13:57:42 +0100

    tailwind.config and navbar settings

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
     <div className="relative min-h-screen">
          <Image
         className="lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10"
-        src="/hero2.png"
-        width={1000}
-        height={600}
+        src="/bg.jpg"
+        width={1500}
+        height={700}
         alt="hero_bg"
       />
 
         <Navbar />
       <div className="container h-[calc(100vh-120px)] grid items-center">
-        <div className="space-y-4 bg-[#ffffff98] w-fit p-4">
+        <div className="space-y-4 bg-[#ffffff48] w-fit p-4">
           <p className="uppercase font-medium">Wide options of choice</p>
           <h2 className="text-4xl sm:text-6xl font-bold">
             Delicious <span className="text-accent">Food</span>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,15 +8,18 @@ const Navbar = () => {
   return (
     <nav className="container pt-6">
     <div className="flex justify-between items-center">
-      <Image src="/logo.png" width={50} height={50} alt="logo" />
-      <ul className="md:flex gap-8 items-center font-semibold text-[14px] hidden">
+      <Image src="/grid-pizza.png" width={50} height={50} alt="logo" />
+      <ul className="md:flex gap-4 items-center font-semibold text-[14px] hidden text-violet-800">
         <li>Home</li>
         <li>About</li>
         <li>Food</li>
         <li>Dish</li>
         <li>Contact</li>
-        <button className="bg-accent text-white px-6 py-2 rounded-3xl">
+        <button className="bg-orange-600 text-violet-200 px-6 py-2 rounded-3xl">
           SignUp
+        </button>
+        <button className="bg-orange-600 text-violet-200 px-6 py-2 rounded-3xl">
+          Login
         </button>
       </ul>
 
